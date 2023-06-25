1d3273432a121ac95a459c4e964de030187d4c4c
Aditya Kumar Dwivedi <129512051+akdwivedi7355@users.noreply.github.com>
2023-06-25 13:53:55 +0530

    Add files via upload

---
+++ A1_hello.java
@@ -0,0 +1,17 @@
+
+public class A1_hello {
+     public static void main(String[] args) {
+    System.out.println("hello.java");
+    System.out.print("Hello Aditya \n");
+    System.out.print("hello coding \n");
+    //print pattern
+    /*
+     * *
+     * * *
+     * * * */
+    System.out.print("* \n** \n*** \n**** \n");
+     }
+}
+// print - for print a statement
+// println - print and take a next line
+// \n - for next line
